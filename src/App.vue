c46626161309c5276e995f54f2279ee2bd9a3be4
Ethan66 <425734130@qq.com>
2017-11-08 21:14:44 +0800

    任务八完成

--- src/App.vue
+++ src/App.vue
@@ -19,14 +19,18 @@
   import ResumeEditor from './components/ResumeEditor'
   import ResumePreview from './components/ResumePreview'
   import icons from './assets/icons'
-export default {
-  name: 'app',
-  components: {Topbar, ResumeEditor, ResumePreview},
-  created(){
-        document.body.insertAdjacentHTML('afterbegin', icons) //
-      }
-
-}
+  export default {
+      name: 'app',
+        data: function () {
+          return {
+              text: '你好'
+          }
+        },
+      components: { Topbar, ResumeEditor, ResumePreview },
+      created() {
+          document.body.insertAdjacentHTML('afterbegin', icons) //
+  }
+  }
 </script>
 
 <style lang="scss">
@@ -53,4 +57,21 @@ export default {
          align-self: center;
        }
       }
+
+#resumeEditor{
+  width: 35%;
+  background: #444;
+}
+#resumePreview{
+  flex-grow: 1;
+  margin-left: 16px;
+  background: #777;
+}
+  svg.icon{
+    height: 1em;
+    width: 1em;
+    fill: currentColor;
+    vertical-align: -0.1em;
+    font-size:16px;
+  }
 </style>
